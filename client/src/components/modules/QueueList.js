3ecf469d113113742e5d9f194d2ea6673ef003f1
Cory Lynch <cjl2625@gmail.com>
2021-03-21 21:52:20 -0400

    Add filtering

---
+++ client/src/components/modules/QueueList.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import { Layout, Button, List } from "antd";
+import ModeIcon from "./ModeIcon";
+
+function QueueList({ title, queues }) {
+  return (
+    <List
+      className="Home-list"
+      header={<div>{title}</div>}
+      size="large"
+      bordered
+      loading={!queues.length}
+      dataSource={queues}
+      renderItem={(item) => (
+        <List.Item key={item._id}>
+          <List.Item.Meta
+            title={<a href={`/${item.owner}`}>{item.owner} </a>}
+            description={
+              <span>
+                {item.modderType === "modder" ? "Modder " : "Beatmap Nominator "}
+                {item.modes.map((m) => (
+                  <ModeIcon key={m} mode={m} size={14} padding={2} />
+                ))}
+              </span>
+            }
+          />
+
+          <div>{item.open ? "Open" : "Closed"}</div>
+        </List.Item>
+      )}
+    />
+  );
+}
+
+export default QueueList;
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -3,10 +3,13 @@ import "../../utilities.css";
 import "./Home.css";
 
 import { navigate } from "@reach/router";
-import { Layout, Button, List } from "antd";
+import { Layout, Button, Radio } from "antd";
 import { get, post } from "../../utilities";
+import QueueList from "../modules/QueueList";
 import ModeIcon from "../modules/ModeIcon";
 const { Content } = Layout;
+
+const isBN = (q) => q.modderType === "full" || q.modderType === "probation";
 
 class Home extends Component {
   constructor(props) {
@@ -31,6 +34,34 @@ class Home extends Component {
     this.setState({ creating: false });
   };
 
+  filterQueues = (queue) => {
+    let ok = true;
+    if (this.state.mode) {
+      console.log(queue.modes);
+      console.log(this.state.mode);
+      ok = ok && queue.modes.includes(this.state.mode);
+      console.log(ok);
+    }
+
+    if (this.state.modderType === "bn") {
+      ok = ok && isBN(queue);
+    } else if (this.state.modderType) {
+      ok = ok && queue.modderType === this.state.modderType;
+    }
+
+    return ok;
+  };
+
+  sortQueues = (a, b) => {
+    if (a.open && !b.open) return -1;
+    if (!a.open && b.open) return 1;
+
+    if (isBN(a) && !isBN(b)) return -1;
+    if (!isBN(a) && isBN(b)) return 1;
+
+    return 0;
+  };
+
   render() {
     return (
       <Content className="u-flex-justifyCenter">
@@ -45,31 +76,26 @@ class Home extends Component {
             )}
           </div>
 
-          <List
-            className="Home-list"
-            header={<div>Modding Queues</div>}
-            size="large"
-            bordered
-            loading={!this.state.queues.length}
-            dataSource={this.state.queues}
-            renderItem={(item) => (
-              <List.Item key={item._id}>
-                <List.Item.Meta
-                  title={<a href={`/${item.owner}`}>{item.owner} </a>}
-                  description={
-                    <span>
-                      {item.modderType === "modder" ? "Modder " : "Beatmap Nominator "}
-                      {item.modes.map((m) => (
-                        <ModeIcon key={m} mode={m} size={14} padding={2} />
-                      ))}
-                    </span>
-                  }
-                />
+          <div className="Home-filters">
+            <span>Filters</span>
+            <Radio.Group onChange={(e) => this.setState({ mode: e.target.value })}>
+              <Radio.Button value="Standard">Standard</Radio.Button>
+              <Radio.Button value="Taiko">Taiko</Radio.Button>
+              <Radio.Button value="Catch the Beat">Catch</Radio.Button>
+              <Radio.Button value="Mania">Mania</Radio.Button>
+            </Radio.Group>
+            <Radio.Group onChange={(e) => this.setState({ modderType: e.target.value })}>
+              <Radio.Button value="bn">Beatmap Nominators</Radio.Button>
+              <Radio.Button value="modder">Modders</Radio.Button>
+            </Radio.Group>
+          </div>
 
-                <div>{item.open ? "Open" : "Closed"}</div>
-              </List.Item>
-            )}
-          />
+          <div className="Home-queues">
+            <QueueList
+              title={"Modding Queues"}
+              queues={this.state.queues.filter(this.filterQueues).sort(this.sortQueues)}
+            />
+          </div>
         </div>
       </Content>
     );
