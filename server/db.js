2bcf6c4df839e8ed4169d0dc867bc9c9a81b29d7
Cory Lynch <cjl2625@gmail.com>
2023-07-28 20:23:40 -0700

    Add retries to db connection

--- server/db.js
+++ server/db.js
@@ -2,13 +2,20 @@ const mongoose = require("mongoose"); // library to connect to MongoDB
 const logger = require("pino")(); // import pino logger
 
 module.exports = {
-  init: () => {
-    // connect to mongodb
-    mongoose.set("strictQuery", true);
-    return mongoose
-      .connect(process.env.MONGO_SRV, {})
-      .then(() => logger.info("Server connected to MongoDB"));
-    //.catch((err) => logger.error("Error connecting to MongoDB", err));
+  init: async () => {
+    while (true) {
+      try {
+        mongoose.set("strictQuery", true);
+        await mongoose.connect(process.env.MONGO_SRV, {});
+        logger.info("Server connected to MongoDB");
+        return mongoose.connection.getClient();
+      } catch (err) {
+        logger.error("Error connecting to MongoDB");
+        logger.error(err);
+        logger.info("Retrying connection in 10 seconds");
+        await new Promise((resolve) => setTimeout(resolve, 10000));
+      }
+    }
   },
   getConnection: () => mongoose.connection,
 };
